69ec9b84939e8b11c2a6038963ae1feb7244bd9f
kirantidke <kirantidke2013@.com>
2021-06-18 17:44:05 +0530

    refactors the code

---
+++ src/Bridgelabz/EmployeeWageUC7.java
@@ -0,0 +1,45 @@
+package Bridgelabz;
+
+public class EmployeeWageUC7 {
+	public static final int IS_PART_TIME = 1;
+	public static final int IS_FULL_TIME = 2;
+	public static final int EMP_RATE_PER_HOUR = 20;
+	public static final int NUMBER_OF_WORKING_DAYS = 2;
+	public static final int MAX_HRS_IN_MONTH = 10;
+	
+	public static int computeEmpWage() {
+		int empHrs = 0;
+		int empWage = 0;
+		int totalEmpWage = 0;
+		int totalEmpHrs = 0;
+		int totalWorkingDays = 0;
+		while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUMBER_OF_WORKING_DAYS) {
+			totalWorkingDays++;
+			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+			switch (empCheck) {
+				case IS_PART_TIME :
+					empHrs = 4;
+					break;
+				case IS_FULL_TIME:
+					empHrs = 8;
+					break;
+					
+				default:
+					empHrs = 0;
+			}
+			//empWage = empHrs * EMP_RATE_PER_HOUR;
+			totalEmpWage += empWage;
+			System.out.println("Day = " + totalWorkingDays + "emp hr = "+empHrs);
+
+		}
+		totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+		System.out.println("total Emp wage = " + totalEmpWage);
+		return totalEmpWage;
+
+	}
+	public static void main(String[] args) {
+		
+		computeEmpWage();
+	}
+
+}
